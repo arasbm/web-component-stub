f41fb883c70dd4fd17ecf5c57c7840bfbeec10c4
Aras Balali Moghaddam <arasbm@gmail.com>
2013-10-15 23:20:14 -0700

    initial proof of concept

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-(function(){  
+(function() {
 
-  // Create your component here
-  // http://x-tags.org/docs
-  
-  xtag.register('x-stub', {
+  xtag.register('x-qr', {
     lifecycle: {
-      created: function() {},
+      created: function() {
+        console.log('x-qr has been created', this);
+        this.innerHTML = '<div>' + this.attributes['label'].value + '</div>';
+        new QRCode(this, this.attributes['href'].value);
+      },
       inserted: function() {},
       removed: function() {},
       attributeChanged: function() {}
-    }, 
-    events: { 
-    
+    },
+    events: {
+      'tap:delegate(div)': function() {
+        console.log('x-qr tapped', this);
+      }
     },
     accessors: {
-      
-    }, 
+      href: {
+        set: function() {
+          console.log('x-qr href has changed', this);
+        }
+      }
+    },
     methods: {
-      
     }
   });
 
