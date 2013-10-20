23807c403941842c40572a8ea8f3f94dfbeb0a48
Aras Balali Moghaddam <arasbm@gmail.com>
2013-10-19 17:59:17 -0700

    first working version, but uses text attribute instead of innerHTML

--- src/main.js
+++ src/main.js
@@ -3,60 +3,71 @@
   var qrcode;
   var qrelement;
 
+  // initialize qr settings with default values
+  var settings = {
+    text: 'some address',
+    width: 128,
+    height: 128,
+    colorDark: '#000000',
+    colorLight: '#ffffff',
+    correctLevel: QRCode.CorrectLevel.H
+  };
+
+  /*
+   * load qrcode settings from attributes of provided dom element
+   */
+  function _loadSettings(element) {
+    Array.prototype.slice.call(element.attributes).forEach(function(item) {
+      // convert attribute name to camelCase
+      var name = item.name.replace(/-([a-z])/g, function(g) {
+          return g[1].toUpperCase();
+        });
+      settings[name] = item.value;
+    });
+  };
+
   xtag.register('x-qr', {
     lifecycle: {
       created: function() {
-        qrelement = xtag.createFragment('<div></div>');
-        this.makeCode();
+        //qrelement = xtag.createFragment('<div></div>');
+        _loadSettings(this);
+        qrelement = document.createElement('div');
+        this.appendChild(qrelement);
+        this.refresh();
         console.log('x-qr has been created', this);
       },
       inserted: function() {},
       removed: function() {},
-      attributeChanged: function() {
-        this.makeCode();
+      attributeChanged: function(attr, oldVal) {
+        console.log(attr + ' attribute changed from ' + oldVal);
+        settings[attr] = this.getAttribute(attr);
+        console.log('to ' + settings[attr]);
+        this.refresh();
       }
     },
     events: {
       'tap:delegate(div)': function() {
         console.log('x-qr tapped', this);
       }
+      /*,
+      'change:delegate(div)': function(e) {
+        console.log('»» CHANGE IS HAPPENING øø', this);
+        e.stopPropagation();
+        xtag.fireEvent(e.currentTarget, 'change');
+      }*/
     },
-    accessors: {
-      text: {
-        set: function() {
-          console.log('x-qr href has changed', this);
-        }
-      },
-      width: {
-        set: function() {
-          console.log('width changed', this);
-        }
-      },
-      height: {
-        set: function() {
-          console.log('height changed', this);
-        }
-      },
-      colorDark: {
-        set: function() {
-          console.log('color dark changed', this);
-        }
-      },
-      colorLight: {
-        set: function() {
-          console.log('color light changed', this);
-        }
-      }
-    },
+    accessors: {},
     methods: {
       clear: function() {
         if (qrcode) {
           qrcode.clear();
         }
       },
-      makeCode: function() {
+      refresh: function() {
+        console.log('[re]making the code');
         this.clear();
-        qrcode = new QRCode(qrelement, this.attributes);
+        qrelement.innerHTML = '';
+        qrcode = new QRCode(qrelement, settings);
       }
     }
   });
