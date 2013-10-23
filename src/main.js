9fd432c2f1872d5951136511a55d05fd8d8b9419
Aras Balali Moghaddam <arasbm@gmail.com>
2013-10-22 22:10:55 -0700

    use innerHTML to generate the QR code

--- src/main.js
+++ src/main.js
@@ -13,16 +13,19 @@
     correctLevel: QRCode.CorrectLevel.H
   };
 
-  /*
-   * load qrcode settings from attributes of provided dom element
-   */
+  // load qrcode settings from attributes of provided dom element
   function _loadSettings(element) {
     Array.prototype.slice.call(element.attributes).forEach(function(item) {
       // convert attribute name to camelCase
-      var name = item.name.replace(/-([a-z])/g, function(g) {
-          return g[1].toUpperCase();
-        });
+      var name = _camelCase(item.name);
       settings[name] = item.value;
+    });
+  }
+
+  // convert attr-name to attrName and return it
+  function _camelCase(name) {
+    return name.replace(/-([a-z])/g, function(g) {
+      return g[1].toUpperCase();
     });
   }
 
@@ -30,44 +33,74 @@
     lifecycle: {
       created: function() {
         //qrelement = xtag.createFragment('<div></div>');
-        _loadSettings(this);
-        qrelement = document.createElement('div');
-        this.appendChild(qrelement);
-        this.refresh();
+        var xqr = this;
+        _loadSettings(xqr);
+        xqr.refresh();
         console.log('x-qr has been created', this);
+
+        xtag.addObserver(xqr, 'inserted', function(e) {
+          console.log('inserted this element: ', e);
+        });
+
+        xtag.addObserver(xqr, 'removed', function(e) {
+          if (e.tagName == 'DIV' && e.classList.contains('qrcode-wrapper')) {
+            // QR code has been removed, so regenerate it with new content
+            console.log('qr code was removed, refreshing', e);
+            xqr.refresh();
+          }
+        });
+
       },
-      inserted: function() {},
-      removed: function() {},
       attributeChanged: function(attr, oldVal) {
         console.log(attr + ' attribute changed from ' + oldVal);
-        settings[attr] = this.getAttribute(attr);
-        console.log('to ' + settings[attr]);
-        this.refresh();
+        switch (attr) {
+          case 'text':
+            // text should be set via innerHTML
+            // returning here to avoid recursive loop with refresh
+            return;
+            break;
+          case 'width':
+          case 'height':
+          case 'color-light':
+          case 'color-dark':
+            settings[_camelCase(attr)] = this.getAttribute(attr);
+            // text attribute has been set when element was last refreshed
+            // resetting the innerHTML will trigger a refresh
+            this.innerHTML = this.getAttribute('text');
+            console.log('changed attr to ' + settings[attr]);
+            break;
+        }
       }
     },
     events: {
       'tap:delegate(div)': function() {
         console.log('x-qr tapped', this);
       }
-      /*,
-      'change:delegate(div)': function(e) {
-        console.log('»» CHANGE IS HAPPENING øø', this);
-        e.stopPropagation();
-        xtag.fireEvent(e.currentTarget, 'change');
-      }*/
     },
     accessors: {},
     methods: {
+      // remove all content of this x-qr tag
       clear: function() {
         if (qrcode) {
           qrcode.clear();
         }
+        this.innerHTML = '';
       },
+      // replace the current content of the x-qr tag with a qrcode
+      // the qrcode is generated from the innerHTML content before removing it
       refresh: function() {
-        console.log('[re]making the code');
+        var content = this.innerHTML;
+
+        // store current content in case we need to update attributes later
+        this.setAttribute('text', content);
+
         this.clear();
-        qrelement.innerHTML = '';
+        qrelement = document.createElement('div');
+        qrelement.classList.add('qrcode-wrapper');
+        this.appendChild(qrelement);
+        settings.text = content;
         qrcode = new QRCode(qrelement, settings);
+        console.log('refreshed qrcode');
       }
     }
   });
